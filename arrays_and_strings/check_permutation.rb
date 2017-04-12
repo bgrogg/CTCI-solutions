88003b0212d294e27765bfbc78379c71a6274058
bgrogg <borisgrogg@Boriss-MacBook-Air.local>
2017-04-11 17:46:20 -0700

    arrays and strings

--- arrays_and_strings/check_permutation.rb
+++ arrays_and_strings/check_permutation.rb
@@ -5,17 +5,41 @@
 
 #Solution 1: Sort the strings
 
-def permutation?(string_a, string_b)
-  return false if string_a.length != string_b.length
-
-  content_a, content_b = string_a.chars, string_b.chars
-  content_a.sort == content_b.sort
-end
-
-permutation?("dog", "god") == true
-permutation?("apple", "banana") == false
-permutation?("dog", "God") == false
+# def permutation?(string_a, string_b)
+#   return false if string_a.length != string_b.length
+#
+#   content_a, content_b = string_a.chars, string_b.chars
+#   content_a.sort == content_b.sort
+# end
+#
+# puts permutation?("dog", "god") == true
+# puts permutation?("apple", "banana") == false
+# puts permutation?("dog", "God") == false
 
 
 #Solution 2: Check if the two strings have identical character counts
 #(more efficient)
+
+def permutation?(string_a, string_b)
+  return false if string_a.length != string_b.length
+
+  letters = Array.new(128, 0)
+  idx = 0
+  while idx < string_a.length
+    letters[string_a[idx].ord] += 1
+    idx += 1
+  end
+
+  idx = 0
+  while idx < string_b.length
+    letters[string_b[idx].ord] -= 1
+    return false if letters[string_b[idx].ord] < 0
+    idx += 1
+  end
+
+  true
+end
+
+puts permutation?("dog", "god") == true
+puts permutation?("apple", "banana") == false
+puts permutation?("dog", "God") == false
---
+++ arrays_and_strings/urlify.rb
@@ -0,0 +1,32 @@
+#Write a method to replace all spaces in a string with '%20.' You may
+#assume that the string has sufficient space at the end to hold additional
+#characters, and that you are given the "true" length of the string.
+
+def replace_spaces(string, true_length)
+  space_count = 0
+  idx = 0
+  while idx < true_length
+    space_count += 1 if string[idx] == ' '
+    idx += 1
+  end
+
+  index = true_length + space_count * 2
+
+  i = true_length - 1
+  while i >= 0
+    if string[i] == ' '
+      string[index - 1] = '0'
+      string[index - 2] = '2'
+      string[index - 3] = '%'
+      index = index - 3
+    else
+      string[index - 1] = string[i]
+      index -= 1
+    end
+    i -= 1
+  end
+
+  string
+end
+
+puts replace_spaces("Mr John Smith    ", 13)
